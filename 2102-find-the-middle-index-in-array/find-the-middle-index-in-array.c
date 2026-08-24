b77a3b4a447ee3053a320ca9421b69e36e40d624
ayushsingh046 <bgayush46@gmail.com>
2026-08-24 23:55:54 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (97.74%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.c
@@ -0,0 +1,14 @@
+int findMiddleIndex(int* nums, int numsSize) {
+    int totalSum = 0;
+    for(int i = 0; i<numsSize; i++){
+        totalSum += nums[i];
+    }
+    int leftSum = 0;
+    for(int i = 0; i<numsSize; i++){
+        if(leftSum == totalSum - leftSum - nums[i]){
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    return -1;
+}
