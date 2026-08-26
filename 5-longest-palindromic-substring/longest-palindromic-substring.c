45a0baec8650ad0aab1554118aaa57c38ad1fe58
ayushsingh046 <bgayush46@gmail.com>
2026-08-26 23:51:00 +0530

    Time: 7 ms (64.90%) | Memory: 9 MB (74.63%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.c
@@ -0,0 +1,32 @@
+#include <string.h>
+
+int expandAroundCenter(char* s, int left, int right, int len) {
+    while (left >= 0 && right < len && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1;
+}
+
+char* longestPalindrome(char* s) {
+    if (s == NULL || *s == '\0') return s;
+    
+    int start = 0;
+    int end = 0;
+    int len = strlen(s);
+    
+    for (int i = 0; i < len; i++) {
+        int len1 = expandAroundCenter(s, i, i, len);       
+        int len2 = expandAroundCenter(s, i, i + 1, len);   
+        
+        int max_len = (len1 > len2) ? len1 : len2;
+        
+        if (max_len > (end - start)) {
+            start = i - (max_len - 1) / 2;
+            end = i + max_len / 2;
+        }
+    }
+    
+    s[end + 1] = '\0';
+    return s + start;
+}
